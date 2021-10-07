69819bc61f2457497909e23db42f5a848420e6fd
asmaa mkawi <asmaamkawi@hotmail.co.uk>
2021-10-07 01:17:17 -0400

    commit

---
+++ MechaCarChallenge.R
@@ -0,0 +1,25 @@
+##Deliverable 1:
+
+library(dplyr)
+library(tidyverse)
+
+MechaCar <- read.csv(file='MechaCar_mpg.csv', check.names= F, stringsAsFactors= F)
+
+lm(mpg ~ vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD,data = MechaCar_mpg) #generate multiple linear regression model
+
+summary(lm(mpg ~ vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD,data = MechaCar_mpg)) #generate summary statistics
+
+
+##Deliverable 2:
+  
+Suspension <- read.csv(file='Suspension_coil.csv',check.names=F,stringsAsFactors = F)
+
+total_summary<-Suspension_Coil%>% summarize(mean=mean(PSI),median=median(PSI),sd=sd(PSI),variance=var(PSI))
+
+lot_summary<-Suspension_Coil %>% group_by(Manufacturing_Lot) %>% summarize(mean=mean(PSI),median=median(PSI),sd=sd(PSI),variance=var(PSI)) #create summary table
+
+##Deliverable 3:
+
+t.test(Suspension_Coil$PSI, mu = 1,500)
+
+
